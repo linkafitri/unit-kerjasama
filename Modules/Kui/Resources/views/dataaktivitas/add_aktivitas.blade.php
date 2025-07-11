aa0b84c7572b83a3cf3619a647613d8e8670fbd4
linkafitri <linkafitri@gmail.com>
2025-07-11 20:44:47 +0700

    nambah folder dataaktivitas

---
+++ Modules/Kui/Resources/views/dataaktivitas/add_aktivitas.blade.php
@@ -0,0 +1,7 @@
+@extends('adminlte::page')
+@section('title', 'List Menu')
+@section('plugins.Select2', true)
+@section('content_header')
+
+@stop
+
---
+++ Modules/Kui/Resources/views/dataaktivitas/edit_aktivitas.blade.php
@@ -0,0 +1,7 @@
+@extends('adminlte::page')
+@section('title', 'Edit Panduan Kerjasama')
+@section('plugins.Select2', true)
+@section('content_header')
+<h1 class="m-0 text-dark">Edit Panduan Kerjasama</h1>
+@stop
+
